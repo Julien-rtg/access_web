5b1ad51dca5ed170f928ab1824eb3af28ac81093
julien-rtg <julien.rittl@gmail.com>
2024-05-23 14:57:58 +0200

    check opquast

---
+++ app.js
@@ -0,0 +1,8 @@
+function showPassword() {
+  const password = document.getElementById("password");
+  if (password.type === "password") {
+    password.type = "text";
+  } else {
+    password.type = "password";
+  }
+}
